d258a8d3b88dec89a541ed1ca8a70601e1319663
aline vajalegre <alinevajalegre@hotmail.com>
2023-05-24 09:37:11 -0300

    Create arithmetic operators

---
+++ operators.py
@@ -0,0 +1,18 @@
+number_1 = 5
+number_2 = 2
+
+soma = number_1 + number_2
+subtracao = number_1 - number_2
+multiplicacao = number_1 * number_2
+divisao = number_1 / number_2
+divisao_inteira = number_1 // number_2
+modulo = number_1 % number_2 #resto da divisao
+exponenciacao = number_1 ** number_2
+
+print(soma) # 7
+print(subtracao) # 3
+print(multiplicacao)  # 10
+print(divisao) # 2.5
+print(divisao_inteira) # 2
+print(modulo)  # 1
+print(exponenciacao) # 25
