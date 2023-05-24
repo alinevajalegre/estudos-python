f0cbef8938416c67248010349e93eb445caa8580
aline vajalegre <alinevajalegre@hotmail.com>
2023-05-24 09:31:41 -0300

    Add output

---
+++ methods-verify-variables.py
@@ -0,0 +1,9 @@
+variable = input('Type something:')
+
+print('É alfanumerico? -->', variable.isalnum())
+print('É ASCII? -->', variable.isascii())
+print('Method returns True if all characters in the string are alphanumeric ---> ', variable.isalnum())
+print('Está toda em caracteres minusculos -->', variable.islower())
+print('Está toda em caracteres maiusculos -->', variable.isupper())
+print('É espaço -->', variable.isspace())
+
