c9c803422de2232d1a10f48c7a739478ef7bd76f
aline vajalegre <alinevajalegre@hotmail.com>
2023-05-23 22:39:02 -0300

    Add output

--- fundamentos.py
+++ fundamentos.py
@@ -1,14 +1,21 @@
 # Saida de dados na tela
 
-print('Hello, World') #Imprime string
-print(10+1) #Imprime 11
-print('7'+'1') #Concatena 7 e 1 -> 71
-print('Aline', 7) #Concatena aline e 7 -> Aline 7
+print('Hello, World')  # Imprime string
+print(10 + 1)  # Imprime 11
+print('7' + '1')  # Concatena 7 e 1 -> 71
+print('Aline', 7)  # Concatena aline e 7 -> Aline 7
 
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
 
-#entrada de dados - input
+print('A soma entre {0} e {1} vale {2}'.format(n1,n2,s))
+
+# entrada de dados - input
 dia = input('Qual é o dia do seu aniversario ')
 mes = input('Qual é o mes do seu aniversario ')
 ano = input('Qual é o ano do seu aniversario ')
 
 print(dia + '/' + mes + '/' + ano)
+
+
