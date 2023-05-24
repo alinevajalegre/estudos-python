6c42dc5cb00af5a4ca6ab1efcff47d188fb81c45
aline vajalegre <alinevajalegre@hotmail.com>
2023-05-23 22:33:49 -0300

    create variables file

---
+++ fundamentos.py
@@ -0,0 +1,14 @@
+# Saida de dados na tela
+
+print('Hello, World') #Imprime string
+print(10+1) #Imprime 11
+print('7'+'1') #Concatena 7 e 1 -> 71
+print('Aline', 7) #Concatena aline e 7 -> Aline 7
+
+
+#entrada de dados - input
+dia = input('Qual é o dia do seu aniversario ')
+mes = input('Qual é o mes do seu aniversario ')
+ano = input('Qual é o ano do seu aniversario ')
+
+print(dia + '/' + mes + '/' + ano)
