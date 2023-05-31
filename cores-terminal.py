09dbeaecad377c1c37d22432957559cda7919b5e
aline vajalegre <alinevajalegre@hotmail.com>
2023-05-31 15:17:31 -0300

    Colors in terminal

---
+++ cores-terminal.py
@@ -0,0 +1,38 @@
+print('\033[4;30;45mBeurick\033[m')
+
+print('\033[31m'+'Isto eh vermelho'+'\033[0;0m')
+#Isto é vermelho
+
+print('\033[32m'+'Isto eh verde'+'\033[0;0m')
+#Isto é verde
+
+print('\033[42m'+'\033[1m'+'\033[33m'+'Isto eh amarelo negrito com fundo verde'+'\033[0;0m')
+#Isto eh amarelo negrito com fundo verde
+
+"""
+
+vermelho = '\033[31m'
+verde = '\033[32m'
+azul = '\033[34m'
+
+ciano = '\033[36m'
+magenta = '\033[35m'
+amarelo = '\033[33m'
+preto = '\033[30m'
+
+branco = '\033[37m'
+
+restaura cor original = '\033[0;0m'
+negrito = '\033[1m'
+reverso = '\033[2m'
+
+fundo preto = '\033[40m'
+fundo vermelho = '\033[41m'
+fundo verde = '\033[42m'
+fundo amarelo = '\033[43m'
+fundo azul = '\033[44m'
+fundo magenta = '\033[45m'
+fundo ciano = '\033[46m'
+fundo branco = '\033[47m'
+
+"""
