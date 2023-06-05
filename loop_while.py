b1b472939045a25ef857c756166135ba064e3fa0
aline vajalegre <alinevajalegre@hotmail.com>
2023-06-05 19:18:02 -0300

    loops using while

---
+++ loop_while.py
@@ -0,0 +1,18 @@
+
+c = 1
+
+while c < 10:
+    print(c)
+    c += 1
+
+for c in range(1, 5):
+    n = int(input('Digite um valor: '))
+print('Fim')
+
+r = 'S'
+while r in 'Ss':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] '))
+print('Fim')
+
+
